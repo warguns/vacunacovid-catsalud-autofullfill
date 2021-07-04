cbda801cf95a418323776790530723c04259848a
warguns <cbargans@gmail.com>
2021-07-04 18:01:30 +0200

    the time-saver script

---
+++ script.js
@@ -0,0 +1,29 @@
+function doTheMagic() {
+    const dni = '{your NIE/DNI/PASSPORT}';
+    const cip = null; // put here your cip if you want to use it instead
+    const phone = '{put here your phone number}';
+    const name = '{put here your name}';
+    const surname = '{put here your surname}';
+    const surname2 = '{put here your surname2}';
+    const mail = '{put here your mail}';
+
+    
+    document.querySelector('vaccinapp-app').shadowRoot.querySelector('uxl-content-switcher').querySelector('vaccinapp-shell').shadowRoot.querySelector('uxl-content-switcher').querySelector('appointment-shell').shadowRoot.querySelector('uxl-content-switcher').querySelector('appointment-onboarding').shadowRoot.querySelector('#dismiss-btn').click();
+    const userRegistration = document.querySelector('vaccinapp-app').shadowRoot.querySelector('uxl-content-switcher').querySelector('vaccinapp-shell').shadowRoot.querySelector('uxl-content-switcher').querySelector('appointment-shell').shadowRoot.querySelector('uxl-content-switcher').querySelector('appointment-user-registration').shadowRoot;
+    if (cip == null) {
+        userRegistration.querySelector('mwc-tab-bar').querySelector('#mdc-tab-2').click();
+        userRegistration.querySelector('#documentID').value = dni;
+        userRegistration.querySelector('#cip').remove();
+    } else {
+        userRegistration.querySelector('mwc-tab-bar').querySelector('#mdc-tab-1').click();
+        userRegistration.querySelector('#cip').value = cip;
+        userRegistration.querySelector('#documentID').remove();
+    }
+    userRegistration.querySelector('#phone').value = phone;
+    userRegistration.querySelector('#name').value = name;
+    userRegistration.querySelector('#surname').value = surname;
+    userRegistration.querySelector('#surname2').value = surname2;
+    userRegistration.querySelector('#mail').value = mail;
+    userRegistration.querySelector('#accept-btn').click();
+}
+doTheMagic();
